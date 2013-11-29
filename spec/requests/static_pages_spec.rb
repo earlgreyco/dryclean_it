800132c94ef0dd2fd1281eb45b7941990778750b
Alexander Cheung <alex@juscollege.com>
2013-11-29 22:43:21 +0900

    ensure email is not case sensitive before saving to db

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,24 +4,22 @@ describe "Static pages" do
   
   subject { page }
 
-	let(:base_title) { "ProblemPop" }
-
   describe "Home page" do
     before { visit root_path }
 
     it { should have_content('ProblemPop') }
-  	it { should have_title("#{base_title}")}
+  	it { should have_title(full_title(''))}
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_title("#{base_title} | About Us") }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_title("#{base_title} | Contact Us") }
+    it { should have_title(full_title('Contact Us')) }
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,12 +4,10 @@ describe "User Pages" do
 
 	subject { page }
 
-	let(:base_title) { "ProblemPop" }
-
   describe "signup page" do
   	before { visit signup_path }
 
   	it { should have_content('Sign up') }
-  	it { should have_title("#{base_title} | Sign up") }
+  	it { should have_title(full_title('Sign up')) }
   end
 end
