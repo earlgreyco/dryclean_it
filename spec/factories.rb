d242ea4b34019e673f62be8845e162db6dc45459
Alexander Cheung <alex@juscollege.com>
2013-12-01 00:39:47 +0900

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
-	factory :user do
-		name "Elon Musk"
-		email "elon@tesla.com"
-		password "model123"
-		password_confirmation "model123"
-	end
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
 end
