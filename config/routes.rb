711b9feaffeae1558dae872aaae49325882a3989
Alexander Cheung <alex@juscollege.com>
2013-11-30 16:49:39 +0900

    ainclude sessionshelper inside applicationcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ ProblemPop::Application.routes.draw do
   #Users
   resources :users
   match '/signup', to: 'users#new', via: 'get'
+
+  #Sessions
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 end
