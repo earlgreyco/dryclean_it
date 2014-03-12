2a49c248c782da3fa5afe742183ebb9968434219
Alex Cheung <alex@juscollege.com>
2014-03-11 20:53:12 -0700

    minor changes to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   	if @user.save
       sign_in @user
   		flash[:success] = "Welcome to The Sarmander!"
-  		redirect_to @user
+  		redirect_to '/my-recipes'
   	else
       render 'new'
   	end
