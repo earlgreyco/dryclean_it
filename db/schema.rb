339651b1512987ba00173b3b5116792e3194a885
Alex Cheung <alex@juscollege.com>
2014-03-19 10:08:37 -0700

    implement spinning wheel - masonry broke for some reason

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140314041011) do
+ActiveRecord::Schema.define(version: 20140314071624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20140314041011) do
   end
 
   add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id", using: :btree
+
+  create_table "invites", force: true do |t|
+    t.string   "email_1"
+    t.string   "email_2"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "recipe_images", force: true do |t|
     t.string   "img"
