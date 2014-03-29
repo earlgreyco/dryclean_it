4fd417c8ac547a45fdd9135ca62d1c2dccc25c8f
Alex Cheung <alex@juscollege.com>
2014-03-29 00:18:38 -0700

    track new articles a day by highcharts

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ActiveRecord::Base
 	belongs_to :order
 	validates_presence_of :order_id, :name, :price, :quantity
+
+	def self.total_on(date)
+    where("date(created_at) = ?", date).sum(:quantity)
+  end
 end
